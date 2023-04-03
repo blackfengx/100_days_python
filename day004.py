51c4403848d100ebcbf12325659cafc866955492
Blackfeng <blackfengsm@gmail.com>
2023-04-03 10:27:48 -0500

    day 4 quiz

--- day004.py
+++ day004.py
@@ -1,7 +1,6 @@
-import random
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 
-names_string = input("give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
-random_index = random.randint(0, len(names)-1)
-random_name = names[random_index]
-print(f"{random_name} is going to buy the meal today!")
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[0])
