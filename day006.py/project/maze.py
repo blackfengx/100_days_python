a61e2094aa0d51ed9f97ae951831e29c92c43792
Blackfeng <blackfengsm@gmail.com>
2023-04-17 13:14:53 -0500

    fixed edge cases on maze

--- day006.py/project/maze.py
+++ day006.py/project/maze.py
@@ -3,10 +3,16 @@
 
 # Read instructions using World Info / Reeborg's Keyboard
 
+# My Solution /////////////////////////////////////////////////////////////////////////////////////
+
 # def turn_right():
 #     turn_left()
 #     turn_left()
 #     turn_left()
+
+# while front_is_clear():
+#     move()
+# turn_left()
 
 # while not at_goal():
 #     if right_is_clear():
