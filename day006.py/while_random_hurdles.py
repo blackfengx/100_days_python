09b0f3a470ef1132be48d09401640a2d4c26a2fa
Blackfeng <blackfengsm@gmail.com>
2023-04-17 11:01:06 -0500

    random hurdle generation solution

---
+++ day006.py/while_random_hurdles.py
@@ -0,0 +1,26 @@
+def hurdle():
+    # turn_left()
+    # move()
+    # turn_right()
+    # move()
+    # turn_right()
+    # move()
+    # turn_left()
+
+def turn_right():
+    # turn_left()
+    # turn_left()
+    # turn_left()
+
+def jump_check():
+    # while not front_is_clear():
+        hurdle()
+
+jump_check()
+
+# while front_is_clear():
+#     if not at_goal():
+#         move()
+#         jump_check()
+#     if at_goal():
+#         done()
