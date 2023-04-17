8e7be4cc776bd29cfa965a2422a41266da27f0b4
Blackfeng <blackfengsm@gmail.com>
2023-04-17 10:29:04 -0500

    hurdle while loop

---
+++ day006.py/while_hurdle.py
@@ -0,0 +1,17 @@
+def hurdle():
+    # move()
+    # turn_left()
+    # move()
+    # turn_right()
+    # move()
+    # turn_right()
+    # move()
+    # turn_left()
+
+def turn_right():
+    # turn_left()
+    # turn_left()
+    # turn_left()
+
+# while not at_goal():
+#     hurdle()
--- testing.py
+++ testing.py
@@ -1,2 +1,7 @@
-for number in range(0, 10):
-    print(number)
+def my_function():
+    a = 3
+    if a > 2:
+        print("This will run")
+my_function()
+
+### Application Developer, Application Integration Engineer, Developer, Infrastructure Engineer, Network Engineer, Software Architect, Software Developer, Software Development Engineer, Software Engineer, Systems Engineer
